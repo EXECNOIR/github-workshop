0b701a50e1e03b7645dd57ce6f9555688a716076
franzsi <franzsi@gmail.com>
2019-01-14 12:59:38 +0100

    c1

---
+++ project/C.cpp
@@ -0,0 +1,11 @@
+//
+// Created by zsfranko on 14/01/2019.
+//
+
+#include "C.h"
+#include <iostream>
+
+void cheer()
+{
+    std::cout << "Penis" << std::endl;
+}
---
+++ project/C.h
@@ -0,0 +1,10 @@
+//
+// Created by zsfranko on 14/01/2019.
+//
+
+#ifndef PROJECT_C_H
+#define PROJECT_C_H
+
+void cheer();
+
+#endif //PROJECT_C_H
