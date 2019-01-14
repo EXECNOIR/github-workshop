9e1d444bb425861db985b5631c9d3f5d2a22074e
EXECNOIR <gump891202@gmail.com>
2019-01-14 12:46:59 +0100

    a1

---
+++ project/A.cpp
@@ -0,0 +1,10 @@
+//
+// Created by EXEC_NOIR on 1/14/2019.
+//
+
+#include "A.h"
+
+void PrintName()
+{
+    std::cout << "Fodor Gabor" << std::endl;
+}
---
+++ project/A.h
@@ -0,0 +1,14 @@
+//
+// Created by EXEC_NOIR on 1/14/2019.
+//
+
+#ifndef PROJECT_A_H
+#define PROJECT_A_H
+
+#include <iostream>
+
+void PrintName();
+
+
+
+#endif //PROJECT_A_H
